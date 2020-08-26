ab2f0943b315985db80834bad5f691cb308dde58
Luiz Antonio Carlin <luiz.carlin@gmail.com>
2020-08-26 17:48:04 -0300

    Spring MVCI - Aula 06
    Conclusão da Aula 06 Spring MVCI - Aula 06

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -19,6 +25,11 @@ public class ProdutosController {
 	
 	@Autowired
 	private ProdutoDAO produtoDao ;
+	
+	@InitBinder
+	public void InitBinder (WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation() );				
+	}
 	
 	@RequestMapping("/form")
 	public ModelAndView form () {
@@ -30,13 +41,16 @@ public class ProdutosController {
 	}	
 	@RequestMapping( method = RequestMethod.POST )
 	//public String grava(String titulo , String descricao, int paginas) {
-	public ModelAndView grava(Produto produto, RedirectAttributes redirectAttributes) {
+	public ModelAndView grava(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
 		System.out.println(".=+=+.=+.=+.=+.=+.=+.=+.=+.=+.=+.=+.=+.=+.=+.");
 //		System.out.println("Titulo......:-> " + titulo);
 //		System.out.println("Descrição...:-> " + descricao);
-//		System.out.println("Páginas.....:-> " + paginas);		
-		System.out.println(produto);
+//		System.out.println("Páginas.....:-> " + paginas);
 		
+		if ( result.hasErrors()) {
+			return form () ;
+		}
+		System.out.println(produto);
 		
 		produtoDao.gravar(produto);
 		
